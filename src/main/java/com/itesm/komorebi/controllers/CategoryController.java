9ebf54644bcb304665b5bce65981268bc8f85441
gusi08 <a01653126@itesm.mx>
2022-05-03 20:08:23 -0500

    Edit of Configuration model and add of configuration service to get the config, categories and tags

--- src/main/java/com/itesm/komorebi/controllers/CategoryController.java
+++ src/main/java/com/itesm/komorebi/controllers/CategoryController.java
@@ -1,35 +1,46 @@
 package com.itesm.komorebi.controllers;
 
 import com.itesm.komorebi.models.Category;
+import com.itesm.komorebi.models.Configuration;
 import com.itesm.komorebi.services.CategoryService;
+import com.itesm.komorebi.services.ConfigurationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/category")
 public class CategoryController {
 
     @Autowired
-    CategoryService categoryService;
+    ConfigurationService configurationService;
 
     @GetMapping("/all")
-    public List<Category> getPerfil(){
-        return categoryService.getAll();
+    public ResponseEntity<?> getAllCategories(){
+        Optional<List<Category>> categoryList = configurationService.getAllCategories();
+        if (categoryList.isEmpty()){
+            return new ResponseEntity("Empty", HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity(categoryList.get(), HttpStatus.OK);
     }
 
-    @GetMapping("/name/{name}")
-    public List<String> getTags(@PathVariable("name") String name){
-        return categoryService.getCategoryTags(name);
+    @GetMapping("/name/{categoryName}")
+    public ResponseEntity<?> getCategory(@PathVariable("categoryName") String categoryName){
+        Optional<Category> category = configurationService.getCategory(categoryName);
+        if (category.isEmpty()){
+            return new ResponseEntity("Do not exist", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(category.get(), HttpStatus.OK);
     }
-
-    @PostMapping("/add")
-    public Category addCategory(@RequestBody Category category){
-        return categoryService.addCategory(category);
-    }
-
-    @PostMapping("/update")
-    public Category updateCategory(@RequestBody Category category){
-        return categoryService.addCategory(category);
+    @GetMapping("/config")
+    public ResponseEntity<?> getConfig(){
+        Optional<Configuration> configuration = configurationService.getConfig();
+        if (configuration.isEmpty()){
+            return new ResponseEntity("Do not exist", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(configuration.get(), HttpStatus.OK);
     }
 }
